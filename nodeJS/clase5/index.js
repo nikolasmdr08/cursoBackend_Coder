e3f94bf6bce38b8ec94631128aecd415629442bf
NicolasMadera <96198896+NicolasMadera@users.noreply.github.com>
2023-01-10 17:34:15 -0300

    fix

--- nodeJS/clase5/index.js
+++ nodeJS/clase5/index.js
@@ -6,6 +6,8 @@ product.imprimir()
 //Prueba class ProductManager
 const Product = require('./product.js')
 const ProductManager = require('./productManager.js')
+
+//alta de items
 const products = new ProductManager()
 products.addProduct("Manzanas", "Fruta", 50, null, 1, 10)
 products.addProduct("Pera", "Fruta", 10, null, 2, 10)
@@ -15,7 +17,7 @@ console.log(products.getProducts())
 //actualizar producto por ID
 let productoUpdate= new Product("Frutilla", "Fruta", 20, null, 5, 10)
 products.updateProduct(2,productoUpdate)
-//console.log(products.getProducts())
+console.log(products.getProducts())
 
 // borrar producto por ID
 products.deleteProduct(3)
